6053d1360096aa768a65ad3763b78dd3c76a4216
jokojiro <jokojiro@yahoo.co.jp>
2021-07-27 00:49:11 +0900

    ユーザー管理

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,21 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
     validates :nickname
     validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'は半角英数字混合での入力が必要です' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'は全角（漢字・平仮名・カタカナ）での入力が必要です' }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'は全角（漢字・平仮名・カタカナ）での入力が必要です' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角（カタカナ）での入力が必要です' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角（カタカナ）での入力が必要です' }
-    validates :birthday
+    
+ 
+    with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角（漢字・ひらがな・カタカナ）文字を使用してください' } do
+    validates :first_name
+    validates :last_name
   end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角（カタカナ）文字を使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
+  validates :birthday
 end
